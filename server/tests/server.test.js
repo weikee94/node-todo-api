30828bc41383609b3fa2df2ddae269f86eee6d9a
WeiKee <weikee070194@gmail.com>
2018-05-01 13:30:31 +0800

    test the DELETE /todos:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -103,4 +103,45 @@ describe('GET /todos:id', () => {
             .expect(404)
             .end(done);
     });
+});
+
+describe('DELETE /todos:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                // query database using findById toNotExist
+                // expect(null).toNotExist();
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toBeFalsy();
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var hexID = new ObjectID().toHexString();
+        request(app)
+            .delete(`/todos/${hexID}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+        .delete('/todos/123abc')
+        .expect(404)
+        .end(done);
+    });
+
 })
